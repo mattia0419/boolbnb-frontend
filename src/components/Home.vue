0f38b3eb14ef33d9a62d9952a42be4e155cc9e2d
mattia0419 <pironemattia6@gmail.com>
2023-11-27 18:10:32 +0100

    searchbox to fix

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<script>
+import { store } from "../data/store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  //   methods: {
+  //     fetchProjects(uri = this.api.baseUrl + "apartment") {
+  //       axios.get(uri).then((responce) => {
+  //         this.apartments = responce.data.data;
+  //       });
+  //     },
+  //   },
+
+  //   created() {
+  //     this.fetchProjects();
+  //   },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="card" v-for="apartment in store.apartments">
+      <div class="card-header">
+        <h3>{{ apartment.title }}</h3>
+      </div>
+      <div class="card-body">
+        <p>{{ apartment.id }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
