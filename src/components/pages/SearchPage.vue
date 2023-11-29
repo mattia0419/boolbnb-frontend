101478c93052a4cb54fbe54728da22c50b471209
mattia0419 <pironemattia6@gmail.com>
2023-11-29 13:07:06 +0100

    add radius to search

--- src/components/pages/SearchPage.vue
+++ src/components/pages/SearchPage.vue
@@ -13,15 +13,45 @@ export default {
     //   console.log(this.store.apartments);
     //   console.log(this.store.searchedAddress);
     // },
+    puntoInCerchio(latPunto, lonPunto, latCentro, lonCentro, raggio) {
+      const raggioTerra = 6371; // Raggio medio della Terra in chilometri
+
+      // Converte le latitudini e longitudini da gradi a radianti
+      const latRadPunto = (latPunto * Math.PI) / 180;
+      const lonRadPunto = (lonPunto * Math.PI) / 180;
+      const latRadCentro = (latCentro * Math.PI) / 180;
+      const lonRadCentro = (lonCentro * Math.PI) / 180;
+
+      // Calcola la distanza tra i due punti usando la formula dell'Earth's haversine
+      const deltaLat = latRadCentro - latRadPunto;
+      const deltaLon = lonRadCentro - lonRadPunto;
+      const a =
+        Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
+        Math.cos(latRadPunto) *
+          Math.cos(latRadCentro) *
+          Math.sin(deltaLon / 2) *
+          Math.sin(deltaLon / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      const distanza = raggioTerra * c;
+
+      return distanza <= raggio;
+    },
 
     filterApartments() {
       for (let i = 0; i < this.store.apartments.length; i++) {
-        if (
-          this.store.apartments[i].latitude ==
-          this.store.searchedAddress.position.lat
-        ) {
+        const puntodacontrollare = this.puntoInCerchio(
+          this.store.apartments[i].latitude,
+          this.store.apartments[i].longitude,
+          this.store.searchedAddress.position.lat,
+          this.store.searchedAddress.position.lon,
+          20
+        );
+        console.log(this.store.apartments[i]);
+        if (puntodacontrollare) {
           this.apartmentsToShow.push(this.store.apartments[i]);
           console.log(this.apartmentsToShow);
+        } else {
+          console.log("Il punto non si trova all'interno del cerchio.");
         }
       }
     },
@@ -52,3 +82,20 @@ export default {
 </template>
 
 <style></style>
+
+<!-- // Utilizzo della funzione per verificare se un punto si trova all'interno del cerchio
+const latitudinePunto = /* Inserisci la latitudine del punto da controllare */;
+const longitudinePunto = /* Inserisci la longitudine del punto da controllare */;
+
+const latitudineCentro = /* Inserisci la latitudine del centro del cerchio */;
+const longitudineCentro = /* Inserisci la longitudine del centro del cerchio */;
+const raggioCerchio = 20; // Raggio del cerchio in chilometri
+
+const puntoDaControllare = puntoInCerchio(latitudinePunto, longitudinePunto, latitudineCentro, longitudineCentro, raggioCerchio);
+
+if (puntoDaControllare) {
+  console.log('Il punto si trova all\'interno del cerchio.');
+} else {
+  console.log('Il punto non si trova all\'interno del cerchio.');
+} -->
+-->
