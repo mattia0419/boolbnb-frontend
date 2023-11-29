f99dfdf5b755582cdc5389460be2a856777870b8
umbertofaenza <umberto.faenza@gmail.com>
2023-11-29 11:26:00 +0100

    filter apartments + print in cards

--- src/components/pages/SearchPage.vue
+++ src/components/pages/SearchPage.vue
@@ -9,17 +9,46 @@ export default {
     };
   },
   methods: {
-    fetchResults() {
-      console.log(this.store.apartments);
-      console.log(this.store.searchedAddress);
+    // logResults() {
+    //   console.log(this.store.apartments);
+    //   console.log(this.store.searchedAddress);
+    // },
+
+    filterApartments() {
+      for (let i = 0; i < this.store.apartments.length; i++) {
+        if (
+          this.store.apartments[i].latitude ==
+          this.store.searchedAddress.position.lat
+        ) {
+          this.apartmentsToShow.push(this.store.apartments[i]);
+          console.log(this.apartmentsToShow);
+        }
+      }
     },
   },
+
   created() {
-    this.fetchResults();
+    // this.logResults();
+    this.filterApartments();
   },
 };
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <div class="row row-cols-3">
+      <div v-for="apartment in this.apartmentsToShow" class="col">
+        <div class="card">
+          <div class="card-header">
+            {{ apartment.title }}
+          </div>
+          <div class="card-body">
+            {{ apartment.address }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style></style>
