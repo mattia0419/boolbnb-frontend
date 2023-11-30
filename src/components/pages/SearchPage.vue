2500b60267e93fd4fb2b23ff5b36a09ec914995e
umbertofaenza <umberto.faenza@gmail.com>
2023-11-30 10:51:24 +0100

    remove unnecessary comment

--- src/components/pages/SearchPage.vue
+++ src/components/pages/SearchPage.vue
@@ -28,20 +28,3 @@ export default {
 </template>
 
 <style></style>
-
-<!-- // Utilizzo della funzione per verificare se un punto si trova all'interno del cerchio
-const latitudinePunto = /* Inserisci la latitudine del punto da controllare */;
-const longitudinePunto = /* Inserisci la longitudine del punto da controllare */;
-
-const latitudineCentro = /* Inserisci la latitudine del centro del cerchio */;
-const longitudineCentro = /* Inserisci la longitudine del centro del cerchio */;
-const raggioCerchio = 20; // Raggio del cerchio in chilometri
-
-const puntoDaControllare = puntoInCerchio(latitudinePunto, longitudinePunto, latitudineCentro, longitudineCentro, raggioCerchio);
-
-if (puntoDaControllare) {
-  console.log('Il punto si trova all\'interno del cerchio.');
-} else {
-  console.log('Il punto non si trova all\'interno del cerchio.');
-} -->
--->
