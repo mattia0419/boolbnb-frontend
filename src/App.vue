75218b7120e1861574177bda151b1aa03d3299ac
umbertofaenza <umberto.faenza@gmail.com>
2023-12-08 12:01:00 +0100

    create featured section and array

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,39 @@ export default {
   data() {
     return {
       store,
+      createdAt: "",
+      days: "",
     };
   },
+
+  methods: {
+    addDays() {
+      var data = new Date(this.createdAt);
+      data.setDate(data.getDate() + this.days);
+      console.log(data);
+    },
+  },
+
   created() {
     axios.get(store.api.baseUrl).then((response) => {
-      store.apartments = response.data.data;
-      console.log(response.data.data);
+      for (let i = 0; i < response.data.data.length; i++) {
+        if (response.data.data[i].sponsorships.length > 0) {
+          // this.createdAt =
+          //   response.data.data[i].sponsorships[0].pivot.created_at;
+          // this.days = response.data.data[i].sponsorships[0].duration;
+          // this.addDays();
+          store.featuredApartments.push(response.data.data[i]);
+        } else {
+          store.apartments.push(response.data.data[i]);
+        }
+      }
+
+      // store.apartments = response.data.data;
+      // console.log(response.data.data);
+      // console.log(store.apartments[0].sponsorships[0].pivot);
     });
   },
+
   components: {
     Navbar,
     router,
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -16,6 +16,25 @@ export default {
     <a href="http://127.0.0.1:8000/admin" class="btn btn-primary my-3"
       >Dashboard</a
     >
+    <h3>Featured Apartments</h3>
+    <div class="row row-cols-4 my-3 g-3">
+      <div class="col" v-for="apartment in store.featuredApartments">
+        <router-link
+          class="nav-link"
+          :to="{ name: 'showdetails', params: { id: `${apartment.id}` } }"
+        >
+          <div class="card">
+            <div class="card-header text-success">
+              <h3>{{ apartment.title }}</h3>
+            </div>
+            <div class="card-body">
+              <p>{{ apartment.id }}</p>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+
     <div class="row row-cols-4 my-3 g-3">
       <div class="col" v-for="apartment in store.apartments">
         <router-link
